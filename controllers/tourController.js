f1bf020922fa55a2dff1020a9b3b27f7876e297e
Ioana-Victoria Dobos <ioana162@yahoo.com>
2022-10-11 14:13:15 +0300

    Move request handler functions from routes to controller files
    Move server configuration to server.js file
    
    Configure project to run on npm start

---
+++ controllers/tourController.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+exports.checkTourId = (req, res, next, val) => {
+  // req.params.id = val
+  const id = req.params.id * 1; // string -> integer
+  const tour = tours.find(el => el.id === id)
+
+  if ( tour === undefined)
+  {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid id"
+    })
+  }
+
+  next();
+}
+
+exports.getAllTours = (req, res) =>
+{
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+}
+
+exports.getTour = (req, res) =>
+{
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tours[req.params.id]
+    }
+  });
+}
+
+exports.createTour = (req, res) =>
+{
+  const newId = tours[tours.length-1].id + 1;
+  const newTour = Object.assign({id: newId}, req.body)
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: "success",
+      data:{
+        newTour } })
+  })
+}
+
+exports.updateTour = (req, res) =>
+{
+  res.status(200).json(
+    {
+      status:"success",
+      data: {
+        tour: "Updated tour here..."
+      }
+    }
+  )
+}
