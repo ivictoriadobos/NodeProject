c14bf6af5a9531ec7cbe8e821f638bbd1e5ef0e0
Ioana-Victoria Dobos <ioana162@yahoo.com>
2022-10-31 13:22:52 +0200

    Add database

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
 const app = require('./app')
+dotenv.config({path : './config.env'});
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(connection => {
+  console.log(connection.connections);
+  console.log('DB connection successful!')
+});
 
 const port = 3000;
 app.listen(port, ()=>
